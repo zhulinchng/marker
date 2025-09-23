54d661d7027f7c54d75056ff172c9f8347ea6c13
Tarun Menta <menta.tarun@gmail.com>
2025-09-23 15:50:40 -0400

    Separate layout and OCR models

--- marker/models.py
+++ marker/models.py
@@ -10,18 +10,15 @@ from surya.layout import LayoutPredictor
 from surya.ocr_error import OCRErrorPredictor
 from surya.recognition import RecognitionPredictor
 from surya.table_rec import TableRecPredictor
+from surya.settings import settings as surya_settings
 
 
 def create_model_dict(
     device=None, dtype=None, attention_implementation: str | None = None
 ) -> dict:
-    foundation_predictor = FoundationPredictor(
-        device=device, dtype=dtype
-    )
     return {
-        "foundation_model": foundation_predictor,
-        "layout_model": LayoutPredictor(foundation_predictor),
-        "recognition_model": RecognitionPredictor(foundation_predictor),
+        "layout_model": LayoutPredictor(FoundationPredictor(checkpoint=surya_settings.LAYOUT_MODEL_CHECKPOINT, attention_implementation=attention_implementation, device=device, dtype=dtype)),
+        "recognition_model": RecognitionPredictor(FoundationPredictor(checkpoint=surya_settings.RECOGNITION_MODEL_CHECKPOINT, attention_implementation=attention_implementation, device=device, dtype=dtype)),
         "table_rec_model": TableRecPredictor(device=device, dtype=dtype),
         "detection_model": DetectionPredictor(device=device, dtype=dtype),
         "ocr_error_model": OCRErrorPredictor(device=device, dtype=dtype),
